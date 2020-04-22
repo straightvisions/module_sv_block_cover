44a4562c8fb7bb083496c740ea2e7045639677e5
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-04-22 14:00:12 +0200

    FEAT - ADD - HAS BLOCK

--- sv_block_cover.php
+++ sv_block_cover.php
@@ -62,12 +62,8 @@
 			return $this;
 		}
 		public function enqueue_scripts(): sv_block_cover {
-			if( ! is_admin() ) {
-				$post = get_post();
-
-				if ( !has_block( 'paragraph', $post )) {
-					return $this;
-				}
+			if(!$this->has_block_frontend('cover')){
+				return $this;
 			}
 
 			$this->get_script( 'common' )->set_is_enqueued();
